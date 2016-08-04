9ccf55b5289b124262acd745f7d54b5a044552f7
Kyle Richardson <skrich3x@gmail.com>
2016-08-03 20:26:17 -0400

    Enqueued Google fonts- Raleway/Open Sans

--- wp-content/themes/freshscores/functions.php
+++ wp-content/themes/freshscores/functions.php
@@ -104,6 +104,9 @@ add_action( 'widgets_init', 'freshscores_widgets_init' );
 function freshscores_scripts() {
 	wp_enqueue_style( 'freshscores-style', get_stylesheet_uri() );
 
+	//Google font
+	wp_enqueue_style( 'freshscores-google-font', 'https://fonts.googleapis.com/css?family=Raleway:400,300|Open+Sans');
+
 	wp_enqueue_script( 'freshscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'freshscores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
